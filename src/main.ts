e222f48ea6f3d67ad9b128a456c0dbd0d93eb238
JunBaam <fkdlsl9088@naver.com>
2021-10-13 14:21:20 +0900

    [Add] nest.js cli

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+// NOTE: 시작점
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
+bootstrap();
